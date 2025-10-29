70056d21356261bcc36e19f5294e191ab181a67d
Fana <fannamehari@gmail.com>
2025-10-29 21:33:37 +0100

    Create a system user to run the Form to Mail service

--- form-to-mail.nix
+++ form-to-mail.nix
@@ -1,6 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
+  users.users.form-to-mail = {
+    isSystemUser = true;
+    group = "form-to-mail";
+    description = "A system user to run the Form to Mail service";
+  };
+  
+  users.groups.form-to-mail = {};
+
   systemd.services.form-to-mail = {
     script = "${pkgs.temurin-jre-bin}/bin/java -jar ~jewiet/form-to-mail-0.1.25-standalone.jar";
     wantedBy = ["multi-user.target"];
