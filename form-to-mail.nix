c8e8dc0eed464ea33c1cb8b9bcee169399fdcd62
Fana <fannamehari@gmail.com>
2025-10-30 11:08:56 +0100

    Run Form to Mail service as form-to-mail user
    For security reasons. This user is unprivileged:
    
      - No `sudo`
      - No login
      - No access to other users' homes.

--- form-to-mail.nix
+++ form-to-mail.nix
@@ -4,14 +4,19 @@
   users.users.form-to-mail = {
     isSystemUser = true;
     group = "form-to-mail";
+    createHome = true;
+    home = "/var/form-to-mail";
     description = "A system user to run the Form to Mail service";
   };
   
   users.groups.form-to-mail = {};
 
   systemd.services.form-to-mail = {
-    script = "${pkgs.temurin-jre-bin}/bin/java -jar ~jewiet/form-to-mail-0.1.25-standalone.jar";
+    script = "${pkgs.temurin-jre-bin}/bin/java -jar ~/form-to-mail-0.1.25-standalone.jar";
     wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      User = "form-to-mail";
+    };
   };
   networking.firewall.allowedTCPPorts = [8080];
 }
